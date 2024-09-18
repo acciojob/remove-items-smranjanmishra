351267e7bd1ccf7218504419c5be370f21418947
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-09-18 20:12:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.getElementById("removeButton").addEventListener("click", function() {
+    // Get the select element
+    const colorSelect = document.getElementById("colorSelect");
+
+    // Remove the selected option from the dropdown
+    colorSelect.remove(colorSelect.selectedIndex);
+});
